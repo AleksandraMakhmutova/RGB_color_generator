cfdb9940e197615b3d1994b19552487abc5307c9
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-12 19:55:02 +0300

    add function gradient for BGC

--- client/src/Component/PartWithColors/index.js
+++ client/src/Component/PartWithColors/index.js
@@ -1,64 +1,90 @@
 import React, { useState } from "react";
 import style from "./style.module.css";
-import { Form } from "react-bootstrap";
-export default function PartWithColors() {
-  const [value, setValue] = useState({
-    red: "255",
-    green: "255",
-    blue: "255",
-  });
+import { Form, Button } from "react-bootstrap";
 
-  function handleChangeValue({ target: { name, value } }) {
-    setValue({ ...value, [name]: value });
+export default function PartWithColors() {
+  const [red, setRed] = useState("255");
+  const [green, setGreen] = useState("255");
+  const [blue, setBlue] = useState("255");
+
+  function handleChangeValue(e) {
+    if (e.target.name == "red") {
+      return setRed(e.target.value);
+    } else if (e.target.name == "green") {
+      return setGreen(e.target.value);
+    } else setBlue(e.target.value);
   }
 
-  const { red, green, blue } = value;
   return (
-    <div className={style.conteiner}>
-      <h2>Result</h2>
-      <input type="text" className={style.box} value="rgb(255,255,255)" />
-      <Form className={style.input}>
-        <Form.Group controlId="formBasicRange">
-          <Form.Label>Red</Form.Label>
-          <Form.Control
-            type="range"
-            id={style.red}
-            value={red}
-            name="red"
-            min="0"
-            max="255"
-            onChange={handleChangeValue}
-          />
-        </Form.Group>
-      </Form>
-      <Form className={style.input}>
-        <Form.Group controlId="formBasicRange">
-          <Form.Label>Green</Form.Label>
-          <Form.Control
-            type="range"
-            id={style.green}
-            value={green}
-            min="0"
-            max="255"
-            name="green"
-            onChange={handleChangeValue}
-          />
-        </Form.Group>
-      </Form>
-      <Form className={style.input}>
-        <Form.Group controlId="formBasicRange">
-          <Form.Label>Blue</Form.Label>
-          <Form.Control
-            type="range"
-            id={style.blue}
-            value={blue}
-            min="0"
-            max="255"
-            name="blue"
-            onChange={handleChangeValue}
-          />
-        </Form.Group>
-      </Form>
-    </div>
+    <>
+      <div className={style.conteiner}>
+        <h2>Result</h2>
+
+        <input
+          type="text"
+          className={style.box}
+          value={`rgb(${red},${green},${blue})`}
+        />
+        <Form className={style.input}>
+          <Form.Group>
+            <Form.Label>Red</Form.Label>
+            <Form.Control
+              type="range"
+              className={style.red}
+              id={style.red}
+              value={red}
+              name="red"
+              min="0"
+              max="255"
+              onChange={handleChangeValue}
+            />
+          </Form.Group>
+        </Form>
+        <Form className={style.input}>
+          <Form.Group controlId="formBasicRange">
+            <Form.Label>Green</Form.Label>
+            <Form.Control
+              type="range"
+              id={style.green}
+              value={green}
+              min="0"
+              max="255"
+              name="green"
+              onChange={handleChangeValue}
+            />
+          </Form.Group>
+        </Form>
+        <Form className={style.input}>
+          <Form.Group controlId="formBasicRange">
+            <Form.Label>Blue</Form.Label>
+            <Form.Control
+              type="range"
+              id={style.blue}
+              value={blue}
+              min="0"
+              max="255"
+              name="blue"
+              onChange={handleChangeValue}
+            />
+          </Form.Group>
+        </Form>
+        <div className={style.btns}>
+          <Button className={style.textColor} variant="outline-success">
+            Text color
+          </Button>
+          <Button variant="outline-warning">BG-Color</Button>
+        </div>
+      </div>
+
+      <div
+        className={style.text}
+        style={{ background: `rgb(${red},${green},${blue})` }}
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus vitae
+        dolorum quisquam, deserunt recusandae harum earum inventore dolor labore
+        magnam, aliquid sit. Aperiam, quidem ullam aliquid doloribus architecto
+        sunt delectus.
+      </div>
+    </>
   );
 }
